d92b071ab80e908fb6eb75f77dfbd7d2976e7b9e
jpe0824 <jason.e24@gmail.com>
2024-11-27 08:08:34 -0700

    setup and landing page

---
+++ pf25_frontend/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { BackgroundLines } from "@/components/ui/background-lines";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+export default function Landing() {
+  const router = useRouter();
+
+  return (
+    <>
+      <BackgroundLines className="flex flex-col items-center justify-center w-full min-h-screen p-6 sm:p-6 md:p-8 lg:p-10">
+        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-6xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          Jason Edman
+        </h2>
+        <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+          Full Stack Software Engineer
+        </p>
+        <div className="py-10">
+          <button
+            type="button"
+            onClick={() => router.push("/home")}
+            className="p-[3px] relative"
+          >
+            <div className="absolute inset-0 bg-gradient-to-r from-lakerpurple to-dodgerblue rounded-lg" />
+            <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+              Continue
+            </div>
+          </button>
+        </div>
+      </BackgroundLines>
+    </>
+  );
+}
